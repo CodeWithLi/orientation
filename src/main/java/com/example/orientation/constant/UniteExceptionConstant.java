0643f10c0ac7d3198317b07a1fbacc4413329682
lzw <2228315291@qq.com>
2024-04-02 23:39:02 +0800

    Revert "APP部分功能的完善"
    This reverts commit e0c5e708

--- src/main/java/com/example/orientation/constant/UniteExceptionConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.orientation.constant;
-
-/**
- * 定义统一的错误
- */
-public class UniteExceptionConstant {
-
-    public static final String ERROR_RESULT="请求参数错误";
-}
--- src/main/java/com/example/orientation/model/po/Admin/AdminStudentPo.java
+++
@@ -1,8 +0,0 @@
-package com.example.orientation.model.po.Admin;
-
-import com.example.orientation.model.parent.AdminStudentParent;
-
-import java.io.Serializable;
-
-public class AdminStudentPo extends AdminStudentParent implements Serializable {
-}
